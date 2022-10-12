e8fab9bbb46fb89ec0ff397733ff68ff69cc8dd9
z-4444 <mzahid04040@gmail.com>
2022-10-12 15:54:21 +0500

    score remove from comments

--- db/migrate/20221012093345_add_scores_to_comments.rb
+++ db/migrate/20221012093345_add_scores_to_comments.rb
@@ -1,5 +1,5 @@
 class AddScoresToComments < ActiveRecord::Migration[6.1]
   def change
-    add_column :comments, :score, :float
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 2022_10_12_093345) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "score"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
